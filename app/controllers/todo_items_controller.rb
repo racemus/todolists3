12718a26cdb2538ea6b22b0fa6852d1f92223f72
Racemus <flasmo@gmail.com>
2015-11-16 03:48:30 +0300

    Showing the number of completed todos

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -5,6 +5,7 @@ class TodoItemsController < ApplicationController
   # GET /todo_items.json
   def index
     @todo_items = TodoItem.all
+    @number_of_completed_todos = TodoItem.count_all_todos
   end
 
   # GET /todo_items/1
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,2 +1,7 @@
 class TodoItem < ActiveRecord::Base
+
+  def self.count_all_todos
+    TodoItem.where(completed: true).count
+  end
+
 end
